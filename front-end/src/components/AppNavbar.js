0ff785185ebe3f7b71563fb6faaa32b957ab2205
Andika Nugraha <m.andika.nugraha@gmail.com>
2018-10-30 17:11:28 +0700

    shopping-list component

--- front-end/src/components/AppNavbar.js
+++ front-end/src/components/AppNavbar.js
@@ -24,20 +24,20 @@ class AppNavbar extends Component {
     render(){
         return(
             <div>
-            <Navbar color="dark" dark expand="sm" className="mb-5">
-                <Container>
-                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="https://www.andika.id">Site</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar>
-        </div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://www.andika.id">Site</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
         );
     }
 }
